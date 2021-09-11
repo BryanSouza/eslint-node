2613257773246ac4d6b4e91135ff8502d1f6fd44
Bryan Souza <bryansouza_01@outlook.com>
2021-09-11 20:59:39 -0300

    add more rules

--- src/server.ts
+++ src/server.ts
@@ -4,5 +4,25 @@
  */
 
 const animal = 'Dragonfly';
-const plant = 'Pinetree';
-console.log(animal, ',', plant);
+const plant = {
+  height: 30,
+  name: 'Pinetree',
+};
+
+console.log(animal, plant);
+
+if (plant.name === '2') {
+  console.log('equal');
+}
+
+console.table({
+  animal: {
+    name: animal,
+    'data type': 'animal',
+  },
+  plant: {
+    name: plant.name,
+    height: String(plant.height),
+    'data type': 'plant',
+  },
+});
